6ac50847c3fd73e2b2523db07246fee7c6a99792
Paweł Czarnecki <pczarnecki@antmicro.com>
2022-02-18 10:26:20 +0100

    ql-qlf: qlf_k6n10f: add licenses to simulation models
    Signed-off-by: Paweł Czarnecki <pczarnecki@antmicro.com>

--- ql-qlf-plugin/qlf_k6n10f/TDP18Kx18_FIFO.v
+++ ql-qlf-plugin/qlf_k6n10f/TDP18Kx18_FIFO.v
@@ -1,3 +1,11 @@
+// Copyright (C) 2020-2021  The SymbiFlow Authors.
+//
+// Use of this source code is governed by a ISC-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/ISC
+//
+// SPDX-License-Identifier:ISC
+
 module TDP18Kx18_FIFO (
 	RMODE_A,
 	RMODE_B,
