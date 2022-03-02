464df532f4f4e60ef0669ef903c350c4d8592692
Paweł Czarnecki <pczarnecki@antmicro.com>
2022-03-02 08:50:14 +0100

    Enable TDP inference
    Signed-off-by: Paweł Czarnecki <pczarnecki@antmicro.com>

--- ql-qlf-plugin/qlf_k6n10f/brams_map.v
+++ ql-qlf-plugin/qlf_k6n10f/brams_map.v
@@ -6,10 +6,11 @@
 //
 // SPDX-License-Identifier:ISC
 
-module \$__QLF_FACTOR_BRAM36_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN);
+module \$__QLF_FACTOR_BRAM36_TDP (A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN, C1ADDR, C1DATA, C1EN, CLK1, CLK2, D1ADDR, D1DATA, D1EN);
 	parameter CFG_ABITS = 10;
 	parameter CFG_DBITS = 36;
 	parameter CFG_ENABLE_B = 4;
+	parameter CFG_ENABLE_D = 4;
 
 	parameter CLKPOL2 = 1;
 	parameter CLKPOL3 = 1;
@@ -22,8 +23,8 @@ module \$__QLF_FACTOR_BRAM36_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DA
 	localparam MODE_2   = 3'b010;	// 32-bit
 	localparam MODE_1   = 3'b001;	// 32-bit
 
+	input CLK1;
 	input CLK2;
-	input CLK3;
 
 	input [CFG_ABITS-1:0] A1ADDR;
 	output [CFG_DBITS-1:0] A1DATA;
@@ -33,20 +34,13 @@ module \$__QLF_FACTOR_BRAM36_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DA
 	input [CFG_DBITS-1:0] B1DATA;
 	input [CFG_ENABLE_B-1:0] B1EN;
 
-	wire [14:0] A1ADDR_15;
-	wire [14:0] B1ADDR_15;
+	input [CFG_ABITS-1:0] C1ADDR;
+	output [CFG_DBITS-1:0] C1DATA;
+	input C1EN;
 
-	wire [35:0] DOBDO;
-
-	wire [14:CFG_ABITS] A1ADDR_CMPL;
-	wire [14:CFG_ABITS] B1ADDR_CMPL;
-	wire [35:CFG_DBITS] A1DATA_CMPL;
-	wire [35:CFG_DBITS] B1DATA_CMPL;
-
-	wire [14:0] A1ADDR_TOTAL;
-	wire [14:0] B1ADDR_TOTAL;
-	wire [35:0] A1DATA_TOTAL;
-	wire [35:0] B1DATA_TOTAL;
+	input [CFG_ABITS-1:0] D1ADDR;
+	input [CFG_DBITS-1:0] D1DATA;
+	input [CFG_ENABLE_B-1:0] D1EN;
 
 	wire FLUSH1;
 	wire FLUSH2;
@@ -82,150 +76,95 @@ module \$__QLF_FACTOR_BRAM36_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DA
 	reg [23:0] PL_ADDR_o;
 	wire [35:0] PL_DATA_o;
 
-	wire [2:0] WMODE_A1;
-	wire [2:0] WMODE_A2;
-	wire [2:0] WMODE_B1;
-	wire [2:0] WMODE_B2;
-	wire [2:0] RMODE_A1;
-	wire [2:0] RMODE_A2;
-	wire [2:0] RMODE_B1;
-	wire [2:0] RMODE_B2;
+	wire [2:0] WMODE;
+	wire [2:0] RMODE;
 
-	assign A1ADDR_CMPL = {15-CFG_ABITS{1'b0}};
-	assign B1ADDR_CMPL = {15-CFG_ABITS{1'b0}};
+	wire [14:CFG_ABITS] A1ADDR_CMPL = {15-CFG_ABITS{1'b0}};
+	wire [14:CFG_ABITS] B1ADDR_CMPL = {15-CFG_ABITS{1'b0}};
+	wire [14:CFG_ABITS] C1ADDR_CMPL = {15-CFG_ABITS{1'b0}};
+	wire [14:CFG_ABITS] D1ADDR_CMPL = {15-CFG_ABITS{1'b0}};
 
-	assign A1ADDR_TOTAL = {A1ADDR_CMPL, A1ADDR};
-	assign B1ADDR_TOTAL = {B1ADDR_CMPL, B1ADDR};
-	assign A1DATA_TOTAL = {A1DATA_CMPL, A1DATA};
-	assign B1DATA_TOTAL = {B1DATA_CMPL, B1DATA};
+	wire [14:0] A1ADDR_TOTAL = {A1ADDR_CMPL, A1ADDR};
+	wire [14:0] B1ADDR_TOTAL = {B1ADDR_CMPL, B1ADDR};
+	wire [14:0] C1ADDR_TOTAL = {C1ADDR_CMPL, C1ADDR};
+	wire [14:0] D1ADDR_TOTAL = {D1ADDR_CMPL, D1ADDR};
+
+	wire [35:CFG_DBITS] A1DATA_CMPL;
+	wire [35:CFG_DBITS] C1DATA_CMPL;
+
+	wire [35:0] A1DATA_TOTAL = {A1DATA_CMPL, A1DATA};
+	wire [35:0] C1DATA_TOTAL = {C1DATA_CMPL, C1DATA};
+
+	wire [14:0] PORT_A_ADDR;
+	wire [14:0] PORT_B_ADDR;
 
 	case (CFG_DBITS)
 		1: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL;
-			assign B1ADDR_15 = B1ADDR_TOTAL;
-			assign WMODE_A1 = MODE_1;
-			assign WMODE_A2 = MODE_1;
-			assign WMODE_B1 = MODE_1;
-			assign WMODE_B2 = MODE_1;
-			assign RMODE_A1 = MODE_1;
-			assign RMODE_A2 = MODE_1;
-			assign RMODE_B1 = MODE_1;
-			assign RMODE_B2 = MODE_1;
+			assign PORT_A_ADDR = A1EN ? A1ADDR_TOTAL : (B1EN ? B1ADDR_TOTAL : 15'd0);
+			assign PORT_B_ADDR = C1EN ? C1ADDR_TOTAL : (D1EN ? D1ADDR_TOTAL : 15'd0);
+			assign WMODE = MODE_1;
+			assign RMODE = MODE_1;
 		end
 
 		2: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 1;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 1;
-			assign WMODE_A1 = MODE_2;
-			assign WMODE_A2 = MODE_2;
-			assign WMODE_B1 = MODE_2;
-			assign WMODE_B2 = MODE_2;
-			assign RMODE_A1 = MODE_2;
-			assign RMODE_A2 = MODE_2;
-			assign RMODE_B1 = MODE_2;
-			assign RMODE_B2 = MODE_2;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 1) : (B1EN ? (B1ADDR_TOTAL << 1) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 1) : (D1EN ? (D1ADDR_TOTAL << 1) : 15'd0);
+			assign WMODE = MODE_2;
+			assign RMODE = MODE_2;
 		end
 
 		4: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 5;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 5;
-			assign WMODE_A1 = MODE_4;
-			assign WMODE_A2 = MODE_4;
-			assign WMODE_B1 = MODE_4;
-			assign WMODE_B2 = MODE_4;
-			assign RMODE_A1 = MODE_4;
-			assign RMODE_A2 = MODE_4;
-			assign RMODE_B1 = MODE_4;
-			assign RMODE_B2 = MODE_4;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 5) : (B1EN ? (B1ADDR_TOTAL << 5) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 5) : (D1EN ? (D1ADDR_TOTAL << 5) : 15'd0);
+			assign WMODE = MODE_4;
+			assign RMODE = MODE_4;
 		end
 
 		8: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 3;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 3;
-			assign WMODE_A1 = MODE_9;
-			assign WMODE_A2 = MODE_9;
-			assign WMODE_B1 = MODE_9;
-			assign WMODE_B2 = MODE_9;
-			assign RMODE_A1 = MODE_9;
-			assign RMODE_A2 = MODE_9;
-			assign RMODE_B1 = MODE_9;
-			assign RMODE_B2 = MODE_9;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 3) : (B1EN ? (B1ADDR_TOTAL << 3) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 3) : (D1EN ? (D1ADDR_TOTAL << 3) : 15'd0);
+			assign WMODE = MODE_9;
+			assign RMODE = MODE_9;
 		end
 
 		9: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 3;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 3;
-			assign WMODE_A1 = MODE_9;
-			assign WMODE_A2 = MODE_9;
-			assign WMODE_B1 = MODE_9;
-			assign WMODE_B2 = MODE_9;
-			assign RMODE_A1 = MODE_9;
-			assign RMODE_A2 = MODE_9;
-			assign RMODE_B1 = MODE_9;
-			assign RMODE_B2 = MODE_9;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 3) : (B1EN ? (B1ADDR_TOTAL << 3) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 3) : (D1EN ? (D1ADDR_TOTAL << 3) : 15'd0);
+			assign WMODE = MODE_9;
+			assign RMODE = MODE_9;
 		end
 
 		16: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 4;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 4;
-			assign WMODE_A1 = MODE_18;
-			assign WMODE_A2 = MODE_18;
-			assign WMODE_B1 = MODE_18;
-			assign WMODE_B2 = MODE_18;
-			assign RMODE_A1 = MODE_18;
-			assign RMODE_A2 = MODE_18;
-			assign RMODE_B1 = MODE_18;
-			assign RMODE_B2 = MODE_18;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 4) : (B1EN ? (B1ADDR_TOTAL << 4) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 4) : (D1EN ? (D1ADDR_TOTAL << 4) : 15'd0);
+			assign WMODE = MODE_18;
+			assign RMODE = MODE_18;
 		end
 
 		18: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 4;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 4;
-			assign WMODE_A1 = MODE_18;
-			assign WMODE_A2 = MODE_18;
-			assign WMODE_B1 = MODE_18;
-			assign WMODE_B2 = MODE_18;
-			assign RMODE_A1 = MODE_18;
-			assign RMODE_A2 = MODE_18;
-			assign RMODE_B1 = MODE_18;
-			assign RMODE_B2 = MODE_18;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 4) : (B1EN ? (B1ADDR_TOTAL << 4) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 4) : (D1EN ? (D1ADDR_TOTAL << 4) : 15'd0);
+			assign WMODE = MODE_18;
+			assign RMODE = MODE_18;
 		end
 
 		32: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 5;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 5;
-			assign WMODE_A1 = MODE_36;
-			assign WMODE_A2 = MODE_36;
-			assign WMODE_B1 = MODE_36;
-			assign WMODE_B2 = MODE_36;
-			assign RMODE_A1 = MODE_36;
-			assign RMODE_A2 = MODE_36;
-			assign RMODE_B1 = MODE_36;
-			assign RMODE_B2 = MODE_36;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 5) : (B1EN ? (B1ADDR_TOTAL << 5) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 5) : (D1EN ? (D1ADDR_TOTAL << 5) : 15'd0);
+			assign WMODE = MODE_36;
+			assign RMODE = MODE_36;
 		end
 		36: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL << 5;
-			assign B1ADDR_15 = B1ADDR_TOTAL << 5;
-			assign WMODE_A1 = MODE_36;
-			assign WMODE_A2 = MODE_36;
-			assign WMODE_B1 = MODE_36;
-			assign WMODE_B2 = MODE_36;
-			assign RMODE_A1 = MODE_36;
-			assign RMODE_A2 = MODE_36;
-			assign RMODE_B1 = MODE_36;
-			assign RMODE_B2 = MODE_36;
+			assign PORT_A_ADDR = A1EN ? (A1ADDR_TOTAL << 5) : (B1EN ? (B1ADDR_TOTAL << 5) : 15'd0);
+			assign PORT_B_ADDR = C1EN ? (C1ADDR_TOTAL << 5) : (D1EN ? (D1ADDR_TOTAL << 5) : 15'd0);
+			assign WMODE = MODE_36;
+			assign RMODE = MODE_36;
 		end
 		default: begin
-			assign A1ADDR_15 = A1ADDR_TOTAL;
-			assign B1ADDR_15 = B1ADDR_TOTAL;
-			assign WMODE_A1 = MODE_36;
-			assign WMODE_A2 = MODE_36;
-			assign WMODE_B1 = MODE_36;
-			assign WMODE_B2 = MODE_36;
-			assign RMODE_A1 = MODE_36;
-			assign RMODE_A2 = MODE_36;
-			assign RMODE_B1 = MODE_36;
-			assign RMODE_B2 = MODE_36;
+			assign PORT_A_ADDR = A1EN ? A1ADDR_TOTAL : (B1EN ? B1ADDR_TOTAL : 15'd0);
+			assign PORT_B_ADDR = C1EN ? C1ADDR_TOTAL : (D1EN ? D1ADDR_TOTAL : 15'd0);
+			assign WMODE = MODE_36;
+			assign RMODE = MODE_36;
 		end
 	endcase
 
@@ -258,46 +197,46 @@ module \$__QLF_FACTOR_BRAM36_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DA
 	assign PL_DATA_i = 36'b0;
 
 	TDP_BRAM36 #() _TECHMAP_REPLACE_ (
-		.WMODE_A1_i(WMODE_A1),
-		.WMODE_A2_i(WMODE_A2),
-		.RMODE_A1_i(RMODE_A1),
-		.RMODE_A2_i(RMODE_A2),
+		.WMODE_A1_i(WMODE),
+		.WMODE_A2_i(WMODE),
+		.RMODE_A1_i(RMODE),
+		.RMODE_A2_i(RMODE),
 
-		.WDATA_A1_i(18'h3FFFF),
-		.WDATA_A2_i(18'h3FFFF),
+		.WDATA_A1_i(B1DATA[17:0]),
+		.WDATA_A2_i(B1DATA[35:18]),
 		.RDATA_A1_o(A1DATA_TOTAL[17:0]),
 		.RDATA_A2_o(A1DATA_TOTAL[35:18]),
-		.ADDR_A1_i(A1ADDR_15),
-		.ADDR_A2_i(A1ADDR_15),
-		.CLK_A1_i(CLK2),
-		.CLK_A2_i(CLK2),
+		.ADDR_A1_i(PORT_A_ADDR),
+		.ADDR_A2_i(PORT_A_ADDR),
+		.CLK_A1_i(CLK1),
+		.CLK_A2_i(CLK1),
 		.REN_A1_i(A1EN),
 		.REN_A2_i(A1EN),
-		.WEN_A1_i(1'b0),
-		.WEN_A2_i(1'b0),
-		.BE_A1_i({A1EN, A1EN}),
-		.BE_A2_i({A1EN, A1EN}),
+		.WEN_A1_i(B1EN[0]),
+		.WEN_A2_i(B1EN[0]),
+		.BE_A1_i({B1EN[1],B1EN[0]}),
+		.BE_A2_i({B1EN[3],B1EN[2]}),
 
 
-		.WMODE_B1_i(WMODE_B1),
-		.WMODE_B2_i(WMODE_B2),
-		.RMODE_B1_i(RMODE_B1),
-		.RMODE_B2_i(RMODE_B2),
+		.WMODE_B1_i(WMODE),
+		.WMODE_B2_i(WMODE),
+		.RMODE_B1_i(RMODE),
+		.RMODE_B2_i(RMODE),
 
-		.WDATA_B1_i(B1DATA[17:0]),
-		.WDATA_B2_i(B1DATA[35:18]),
-		.RDATA_B1_o(DOBDO[17:0]),
-		.RDATA_B2_o(DOBDO[35:18]),
-		.ADDR_B1_i(B1ADDR_15),
-		.ADDR_B2_i(B1ADDR_15),
-		.CLK_B1_i(CLK3),
-		.CLK_B2_i(CLK3),
-		.REN_B1_i(1'b0),
-		.REN_B2_i(1'b0),
-		.WEN_B1_i(B1EN[0]),
-		.WEN_B2_i(B1EN[0]),
-		.BE_B1_i(B1EN[1:0]),
-		.BE_B2_i(B1EN[3:2]),
+		.WDATA_B1_i(D1DATA[17:0]),
+		.WDATA_B2_i(D1DATA[35:18]),
+		.RDATA_B1_o(C1DATA_TOTAL[17:0]),
+		.RDATA_B2_o(C1DATA_TOTAL[35:18]),
+		.ADDR_B1_i(PORT_B_ADDR),
+		.ADDR_B2_i(PORT_B_ADDR),
+		.CLK_B1_i(CLK2),
+		.CLK_B2_i(CLK2),
+		.REN_B1_i(C1EN),
+		.REN_B2_i(C1EN),
+		.WEN_B1_i(D1EN[0]),
+		.WEN_B2_i(D1EN[0]),
+		.BE_B1_i({D1EN[1],D1EN[0]}),
+		.BE_B2_i({D1EN[3],D1EN[2]}),
 
 
 		.SPLIT_i(SPLIT),
