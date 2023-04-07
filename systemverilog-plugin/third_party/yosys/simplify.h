f86d06384f2609cac0e12881d2e5b6a8569333b0
Magdalena Andrys <mandrys@antmicro.com>
2023-04-07 17:06:42 +0200

    define size_packed_struct static

--- systemverilog-plugin/third_party/yosys/simplify.h
+++ systemverilog-plugin/third_party/yosys/simplify.h
@@ -3,5 +3,5 @@
 namespace systemverilog_plugin
 {
     bool simplify(Yosys::AST::AstNode *ast_node, bool const_fold, bool at_zero, bool in_lvalue, int stage, int width_hint, bool sign_hint, bool in_param);
-	static int size_packed_struct(Yosys::AST::AstNode *snode, int base_offset);
+	int size_packed_struct(Yosys::AST::AstNode *snode, int base_offset);
 }
